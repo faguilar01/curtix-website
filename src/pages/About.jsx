c7a4bc44dcc731c7b2209147e6102ebffc4f1852
Owner <owner@owners-air-6.mynetworksettings.com>
2026-04-25 15:31:37 -0400

    Fix duplicate code in About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -82,20 +82,3 @@ function About() {
 }
 
 export default About
-            </div>
-          </div>
-          <div className="bg-gray-200 rounded-lg h-96 flex items-center justify-center">
-            <div className="text-center text-gray-500">
-              <svg className="w-24 h-24 mx-auto mb-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-              </svg>
-              <p>Add your business photo here</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
-
-export default About
